a005417d14f72dbf79ef98292ba8431303175b3e
dipanc1 <dipanchhabra@gmail.com>
2023-03-22 10:43:26 +0530

    dist update

--- api/dist/auth/models/friend-request.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { FriendRequest_Status } from "./friend-request.interface";
-import { UserEntity } from "./user.entity";
-export declare class FriendRequestEntity {
-    id: number;
-    receiver: UserEntity;
-    creator: UserEntity;
-    status: FriendRequest_Status;
-}
--- api/dist/feed/models/post.interface.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "src/auth/models/user.class";
-export interface FeedPost {
-    id?: number;
-    body?: string;
-    createdAt?: Date;
-    author?: User;
-}
